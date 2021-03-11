852c09631082de06082a5b0a2650eb6c70c6cc8b
Jason <jason1985@gmail.com>
2021-03-11 07:49:38 +0700

    changed menu text

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let selected = 0
-let list = document.getElementsByTagName('li')
+let list = document.getElementsByTagName('span')
 
 list[0].style.backgroundColor = '#a00'
 
@@ -8,7 +8,7 @@ document.addEventListener('keydown', keydown)
 function keydown(e) {
   switch (e.keyCode) {
     case 37: {
-      //
+      if (selected > 6) selector(selected - 7)
       break
     }
     case 38: {
@@ -16,7 +16,7 @@ function keydown(e) {
       break
     }
     case 39: {
-      //
+      if (selected < 7) selector(selected + 7)
       break
     }
     case 40: {
